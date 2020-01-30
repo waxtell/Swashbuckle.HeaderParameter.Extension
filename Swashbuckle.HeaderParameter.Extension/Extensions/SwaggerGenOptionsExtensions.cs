58337485b7e357b5c69a915151e71dccba5ec525
Will Axtell <waxtell@transtar1.com>
2020-01-30 15:14:36 -0500

    Initial scaffolding

---
+++ Swashbuckle.HeaderParameter.Extension/Extensions/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+// ReSharper disable once CheckNamespace
+namespace Swashbuckle.HeaderParameter.Extension
+{
+    public static class SwaggerGenOptionsExtensions
+    {
+        public static void AddHeaderParameters(this SwaggerGenOptions swaggerGenOptions)
+        {
+            swaggerGenOptions.OperationFilter<HeaderParameterOperationalFilter>();
+        }
+    }
+}
